453476f84aa5c8188cba59fe11b3461cb4beac29
Tesshu Flower <tflower@redhat.com>
2024-08-07 18:45:20 -0400

    colorcolumn set to 120
    Signed-off-by: Tesshu Flower <tflower@redhat.com>

--- lvim/config.lua
+++ lvim/config.lua
@@ -3,6 +3,10 @@
 -- Video Tutorials: https://www.youtube.com/watch?v=sFA9kX-Ud_c&list=PLhoH5vyxr6QqGu0i7tt_XoVK9v-KvZ3m6
 -- Forum: https://www.reddit.com/r/lunarvim/
 -- Discord: https://discord.com/invite/Xb9B4Ny
+--
+
+-- General options
+vim.opt.colorcolumn = "120"
 
 -- gt/gT to switch between tabs
 lvim.keys.normal_mode["gt"] = ":BufferLineCycleNext<CR>"
